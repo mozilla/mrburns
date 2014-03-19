df13e111ba2ee5e9ab08060029d7b926cc4b37bb
Paul McLanahan <pmac@mozilla.com>
2014-03-19 12:41:17 -0400

    Add MPL comment to files.

--- mrburns/urls.py
+++ mrburns/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, include, url
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from django.conf.urls import patterns, url
 
 from mrburns.main.views import GlowView
 
