00bef6cda0643a695aa7a03fd8f0422030225d94
Steven Garrity <steven@silverorange.com>
2014-04-03 19:40:05 -0300

    Add "Share the map" popover and links

--- mrburns/main/static/js/main.js
+++ mrburns/main/static/js/main.js
@@ -71,4 +71,15 @@ $(document).ready(function () {
         $( '#number' ).modal();
     }
 
+    $('.popover-markup > .trigger').popover({
+        html : true,
+        title: function() {
+          return $(this).parent().find('.head').html();
+        },
+        content: function() {
+          return $(this).parent().find('.content').html();
+        },
+        container: 'body'
+    });
+
 });
