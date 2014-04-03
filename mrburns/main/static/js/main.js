fc8feef798de345db633ec7475b2c515ffa66b3d
Steven Garrity <steven@silverorange.com>
2014-04-03 13:16:02 -0300

    Open modal windows depending on URL hashes Logic is as follows:  * If we're on Australis, and there are no hashes, open the #choice modal  * If we're not on Australis, and there are no hashes, do nothing (stay on map)  * If #choice, #video, or #number are in the URL, open the corresponding modal

--- mrburns/main/static/js/main.js
+++ mrburns/main/static/js/main.js
@@ -31,23 +31,43 @@ function isFirefox(userAgent) {
 }
 
 function isAustralis() {
-
-    var fx_version = getFirefoxMasterVersion();
-    var minimumAustralisVersion = 29;
-
-    return (fx_version >= minimumAustralisVersion);
-
+    // Check if the visitor is using Firefox and it's v29+
+    if (isFirefox()) {
+        var fx_version = getFirefoxMasterVersion();
+        var minimumAustralisVersion = 29;
+        return (fx_version >= minimumAustralisVersion);
+    } else {
+        return false;
+    }
 }
 
 $(document).ready(function () {
 
-    if (isFirefox() && isAustralis()) {
+    var hash = window.location.hash;
+
+    if (isAustralis()) {
         // if the browser is Australis, add a class to HTML tag
         // and show the primary choice modal
         var $html = $(document.documentElement);
         $('html').addClass('australis');
+
+        // if we're on australis and there's no hash tag, show the choice modal
+        window.alert(hash.indexOf("#") === -1);
+        if (hash.indexOf("#") === -1) {
+            $( '#choice' ).modal();
+        }
+
+    }
+
+    if (hash.indexOf("choice") != -1) {
+        // if #choice is in the URL, show the choice modal
         $( '#choice' ).modal();
-    } else {
+    } else if (hash.indexOf("video") != -1) {
+        // if #video is in the URL, show the video modal
+        $( '#video' ).modal();
+    } else if (hash.indexOf("number") != -1) {
+        // if #number is in the URL, show the number modal
+        $( '#number' ).modal();
     }
 
 });
