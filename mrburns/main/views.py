3c0d142f03a5dd12e27965dc8aa6e48477a6780e
Paul McLanahan <pmac@mozilla.com>
2014-04-07 15:52:52 -0400

    Add map share urls to view context for twitter and FB.

--- mrburns/main/views.py
+++ mrburns/main/views.py
@@ -3,9 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
+from urllib import urlencode
 
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseNotFound
+from django.utils.translation import ugettext as _
 from django.views.generic import TemplateView, View
 
 from redis_cache import get_redis_connection
@@ -13,9 +15,36 @@ from redis_cache import get_redis_connection
 
 redis = get_redis_connection('smithers')
 
+TWITTER_URL = 'https://twitter.com/share'
+FB_URL = 'https://www.facebook.com/sharer/sharer.php'
+
+
+def get_tw_share_url(params):
+    return '?'.join([TWITTER_URL, urlencode(params)])
+
+
+def get_fb_share_url(params):
+    return '?'.join([FB_URL, urlencode(params)])
+
 
 class GlowView(TemplateView):
     template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GlowView, self).get_context_data(**kwargs)
+        context.update({
+            'share_map_twitter': get_tw_share_url({
+                'url': 'https://webwewant.mozilla.org/#web',
+                'hashtags': '#firefox',
+                'dnt': True,
+                'text': _('Join millions of Firefox users around the world '
+                          'who are shaping the future of the Web.'),
+            }),
+            'share_map_facebook': get_fb_share_url({
+                'u': 'https://webwewant.mozilla.org/#web'
+            }),
+        })
+        return context
 
 
 class ShareView(View):
