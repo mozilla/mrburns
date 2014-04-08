d193f0edc6f39f3e34d11846ff91806d85502b8c
almossawi <almossawi@gmail.com>
2014-04-08 07:18:47 -0700

    Hooked up top issue map view to dev data
    Sorted stacked bar elements on stats page per the order in the
    left-hand side menu

--- mrburns/main/static/js/map.js
+++ mrburns/main/static/js/map.js
@@ -6,14 +6,6 @@ var width,
 var max_simultaneous_glows = 400,
     glow_tick = 60000; //in ms
 
-var color_dummy = new Object();
-    color_dummy["Asia"] = "#d6557a";
-    color_dummy["Africa"] = "#6e3f7d";
-    color_dummy["South America"] = "#1d4488",
-    color_dummy["North America"] = "#6c9e45",
-    color_dummy["Europe"] = "#df9128",
-    color_dummy["Oceania"] = "#be514f";
-
 $(document).ready(function() {
     assignEventListeners();
     drawMap($("#map-container").width() / 2 - 100);
@@ -33,18 +25,34 @@ function assignEventListeners() {
     });
 
     $("#view-by-region").on("click", function() {
+        var top_issues = new Object();
+        
         d3.selectAll(".continent").each(function(d, i) {
+            var country_attribs = data.continent_issues[getContinentCode(d.name)];
+            var top_issue_for_this_continent = d3.entries(country_attribs).sort()[0].key;
+
+            top_issues[getContinentCode(d.name)] = top_issue_for_this_continent;
+        
             d3.select(this).style("fill", function(d, i) {
-                return color_dummy[d.name];
+                return color[top_issue_for_this_continent];
             }).style("stroke", function(d, i) {
-                return color_dummy[d.name];
+                return color[top_issue_for_this_continent];
             });
         });
-
-        addTopIssueLabels();
+        
+        addTopIssueLabels(top_issues);
 
         return false;
     });
+}
+
+function getContinentCode(continent_name) {
+    if(continent_name == "Asia") return "AS";
+    else if(continent_name == "Africa") return "AF";
+    else if(continent_name == "North America") return "NA";
+    else if(continent_name == "South America") return "SA";
+    else if(continent_name == "Oceania") return "OC";
+    else if(continent_name == "Europe") return "EU";
 }
 
 function drawMap(ht) {
@@ -65,7 +73,7 @@ function drawMap(ht) {
 
         //http://geojson.org/geojson-spec.html#introduction
         var asia = {type: "FeatureCollection",name: "Asia",id: 1,features: countries.features.filter(function (d) {return d.properties.continent == "Asia";})};
-    	var africa = {type: "FeatureCollection",name: "Africa",id: 2,features: countries.features.filter(function (d) {return d.properties.continent == "Africa";})};
+        var africa = {type: "FeatureCollection",name: "Africa",id: 2,features: countries.features.filter(function (d) {return d.properties.continent == "Africa";})};
         var europe = {type: "FeatureCollection",name: "Europe",id: 3,features: countries.features.filter(function (d) {return d.properties.continent == "Europe";})};
         var na = {type: "FeatureCollection",name: "North America",id: 4,features: countries.features.filter(function (d) {return d.properties.continent == "North America";})};
         var sa = {type: "FeatureCollection",name: "South America",id: 5,features: countries.features.filter(function (d) {return d.properties.continent == "South America";})};
@@ -96,7 +104,7 @@ function drawMap(ht) {
     });
 }
 
-function addTopIssueLabels() {
+function addTopIssueLabels(top_issues) {
     var top_offset = 0;
 
     var na_position = [width / 5, top_offset + height / 4.6],
@@ -106,22 +114,90 @@ function addTopIssueLabels() {
         asia_position = [width / 1.42, top_offset + height / 3.2],
         oceania_position = [width / 1.2, top_offset + height / 1.46];
 
-    $("#na-top-issue").css("margin-left", na_position[0] + "px").css("margin-top", na_position[1] + "px").html("<div class='header'>Top Issue</div>Freedom");
-    $("#sa-top-issue").css("margin-left", sa_position[0] + "px").css("margin-top", sa_position[1] + "px").html("<div class='header'>Top Issue</div>Accessibility");
-    $("#africa-top-issue").css("margin-left", africa_position[0] + "px").css("margin-top", africa_position[1] + "px").html("<div class='header'>Top Issue</div>Opportunity");
-    $("#asia-top-issue").css("margin-left", asia_position[0] + "px").css("margin-top", asia_position[1] + "px").html("<div class='header'>Top Issue</div>Privacy");
-    $("#europe-top-issue").css("margin-left", europe_position[0] + "px").css("margin-top", europe_position[1] + "px").html("<div class='header'>Top Issue</div>Learning");
-    $("#oceania-top-issue").css("margin-left", oceania_position[0] + "px").css("margin-top", oceania_position[1] + "px").html("<div class='header'>Top Issue</div>User control");
+    $("#na-top-issue")
+        .css("margin-left", na_position[0] + "px")
+        .css("margin-top", na_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["NA"] 
+            + " .choice-title span").html());
+            
+    $("#sa-top-issue")
+        .css("margin-left", sa_position[0] + "px").css("margin-top", sa_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["SA"] 
+            + " .choice-title span").html());
+            
+    $("#africa-top-issue")
+        .css("margin-left", africa_position[0] + "px")
+        .css("margin-top", africa_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["AF"] 
+            + " .choice-title span").html());
+            
+    $("#asia-top-issue")
+        .css("margin-left", asia_position[0] + "px")
+        .css("margin-top", asia_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["AS"] 
+            + " .choice-title span").html());
+    
+    $("#europe-top-issue")
+        .css("margin-left", europe_position[0] + "px")
+        .css("margin-top", europe_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["EU"] 
+            + " .choice-title span").html());
+
+    $("#oceania-top-issue")
+        .css("margin-left", oceania_position[0] + "px")
+        .css("margin-top", oceania_position[1] + "px")
+        .html("<div class='header'>Top Issue</div>" + $(".choice-" + top_issues["OC"] 
+            + " .choice-title span").html());
 }
 
 function populateGlowsFromLastTick(projection, svg) {
     d3.json(getJsonDataUrl(), function(places) {
         $(".share_total").html(addCommas(places.share_total));
         
-        places.map_geo.splice(max_simultaneous_glows, places.map_geo.length-max_simultaneous_glows);
+        //we sort by count to give preference to locations that have the most downloads
+        //dots for locations that have a lot of downloads persist for the entire length of the tick
+        //for now, cycle through subsets of 400 glows every 10s, any more and the animation becomes less graceful
+        //start from the end
+        places.map_geo.sort(function(a, b) { return a.count - b.count; })
         
-        svg.selectAll(".pin")
-            .data(places.map_geo)
+        displaySubsetOfGlows(places.map_geo.splice(places.map_geo.length-max_simultaneous_glows, places.map_geo.length), projection, svg);
+        
+        setTimeout(function() {
+            displaySubsetOfGlows(places.map_geo.splice(
+                places.map_geo.length-max_simultaneous_glows, 
+                    places.map_geo.length), projection, svg);
+        }, 10000);
+        
+        setTimeout(function() {
+            displaySubsetOfGlows(places.map_geo.splice(
+                places.map_geo.length-max_simultaneous_glows, 
+                    places.map_geo.length), projection, svg);
+        }, 20000);
+        
+        setTimeout(function() {
+            displaySubsetOfGlows(places.map_geo.splice(
+                places.map_geo.length-max_simultaneous_glows, 
+                    places.map_geo.length), projection, svg);
+        }, 30000);
+        
+        setTimeout(function() {
+            displaySubsetOfGlows(places.map_geo.splice(
+                places.map_geo.length-max_simultaneous_glows, 
+                    places.map_geo.length), projection, svg);
+        }, 40000);
+        
+        setTimeout(function() {
+            displaySubsetOfGlows(places.map_geo.splice(
+                places.map_geo.length-max_simultaneous_glows, 
+                    places.map_geo.length), projection, svg);
+        }, 50000);
+    });
+}
+
+function displaySubsetOfGlows(places, projection, svg) {
+    console.log("loading subchunk of glows");
+    svg.selectAll(".pin")
+            .data(places)
             .enter().append("circle")
                 .attr("r", 0)
                 .style("opacity", 0)
@@ -130,16 +206,25 @@ function populateGlowsFromLastTick(projection, svg) {
                 })
                 .transition()
                     .delay(function(d, i) {
-                        return randomRange(0, glow_tick, 0);
+                        if(d.count > 30)
+                            return randomRange(0, 2000, 0);
+                        else
+                            return randomRange(0, 10000, 0); //10s is the sub-tick length
                     })
-                    .duration(1000)
+                    .duration(function(d, i) {
+                        return 1000; //show for 1s
+                    })
                     .attr("r", 3)
                     .style("opacity", 0.8)
                     .transition()
-                        .duration(4000)
+                        .duration(function(d, i) {
+                            if(d.count > 30) 
+                                return glow_tick; //show for 60s
+                            else
+                                return 4000;
+                        })
                         .style("opacity", 0)
                         .remove();
-    });
 }
 
 function randomRange(minVal, maxVal, floatVal) {
@@ -148,13 +233,13 @@ function randomRange(minVal, maxVal, floatVal) {
 }
 
 function addCommas(nStr) {
-	nStr += '';
-	var x = nStr.split('.');
-	var x1 = x[0];
-	var x2 = x.length > 1 ? '.' + x[1] : '';
-	var rgx = /(\d+)(\d{3})/;
-	while (rgx.test(x1)) {
-		x1 = x1.replace(rgx, '$1' + ',' + '$2');
-	}
-	return x1 + x2;
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
 }
