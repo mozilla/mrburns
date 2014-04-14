597c1e3a3a2a392443c27287f6fa6e1a39d2ba76
Paul McLanahan <pmac@mozilla.com>
2014-04-14 12:25:37 -0400

    Bump minimum share count for countries to 500.

--- smithers/smithers/conf/base.py
+++ smithers/smithers/conf/base.py
@@ -8,6 +8,6 @@ JSON_OUTPUT_DIR = os.path.join(BASE_DIR, 'JSON')
 GEOIP_DB_FILE = os.path.join(BASE_DIR, 'GeoIP2-City.mmdb')
 LOG_LEVEL = getenv('SMITHERS_LOG_LEVEL', 'INFO')
 
-COUNTRY_MIN_SHARE = 20
+COUNTRY_MIN_SHARE = 500
 
 REDIS_UNIX_SOCKET_PATH = '/var/run/redis/redis.sock'
