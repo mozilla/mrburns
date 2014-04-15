f732868b7dd8ea243fa8bfa03c80eb5b39f5cd73
Paul McLanahan <pmac@mozilla.com>
2014-04-15 13:12:48 -0400

    Remove "stub" from the download log filter
    The stub installer downloads "firefox-latest", so
    we'd be counting those twice.

--- smithers/bart.sh
+++ smithers/bart.sh
@@ -6,7 +6,7 @@ NEW_LOG="/var/log/glowmo/syslog/glow.${EPOCH}.log"
 mv -f /var/log/glow.log ${NEW_LOG}
 kill -HUP `cat /var/run/syslogd.pid`
 
-cat ${NEW_LOG} | grep ' 302 ' | grep -iE 'firefox-(28|latest|stub)' | awk '{print "lpush geoip 0," $3}' >> /tmp/redis-commands.txt
+cat ${NEW_LOG} | grep ' 302 ' | grep -iE 'firefox-(28|latest)' | awk '{print "lpush geoip 0," $3}' >> /tmp/redis-commands.txt
 
 cat /tmp/redis-commands.txt | redis-cli && rm /tmp/redis-commands.txt
 
