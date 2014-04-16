6f43c87cfc21d0d03008f83a2868a80220c43e21
Paul McLanahan <pmac@mozilla.com>
2014-04-16 12:23:43 -0400

    Parameterize firefox version for log filtering.

--- smithers/bart_process_log.sh
+++ smithers/bart_process_log.sh
@@ -6,6 +6,10 @@ if [ -z "$LOG_FILE" ]; then
     exit 1
 fi
 
-cat ${LOG_FILE} | grep ' 302 ' | grep -iE 'firefox-(28|latest)' | awk '{print "lpush geoip 0," $3}' >> /tmp/redis-commands.txt
+if [ -z "${FIREFOX_VERSION}" ]; then
+    FIREFOX_VERSION=28
+fi
+
+cat ${LOG_FILE} | grep ' 302 ' | grep -iE "firefox-(${FIREFOX_VERSION}|latest)" | awk '{print "lpush geoip 0," $3}' >> /tmp/redis-commands.txt
 
 cat /tmp/redis-commands.txt | redis-cli && rm /tmp/redis-commands.txt
