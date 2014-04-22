fd99ecaa545eb4b952e9fe73d76ebfb1d8722284
Steven Garrity <steven@silverorange.com>
2014-04-22 15:22:06 -0300

    Update share links with fixed short-URLs

--- mrburns/main/views.py
+++ mrburns/main/views.py
@@ -68,37 +68,37 @@ class GlowView(TemplateView):
                        'about the Web they want!'),
             ),
             'share_stats_twitter_privacy': get_tw_share_url(
-                url='http://mzl.la/1hq7RRe',
+                url='http://mzl.la/1i6jEol',
                 text='#Firefox {}'
                      .format(_('fights for government surveillance reform '
                                'and was the only major browser not targeted by the NSA scandal.')),
             ),
             'share_stats_twitter_opportunity': get_tw_share_url(
-                url='http://mzl.la/1lQpeeD',
+                url='http://mzl.la/1nEMbBx',
                 text='#Firefox {}'
                      .format(_('is made by a global volunteer community 6,000 strong, '
                                'open to participation from anyone.')),
             ),
             'share_stats_twitter_access': get_tw_share_url(
-                url='http://mzl.la/1lQpudA',
+                url='http://mzl.la/1k45Dae',
                 text='#Firefox {}'
                      .format(_('disrupted the mobile industry with the first Web-based OS to '
                                'help bring the next billion people online')),
             ),
             'share_stats_twitter_freedom': get_tw_share_url(
-                url='http://mzl.la/1qGWpl2',
+                url='http://mzl.la/1rfuE59',
                 text='#Firefox {}'
                      .format(_('protested against SOPA and PIPA, dangerous copyright legislation '
                                'that threatened the freedom of the Web.')),
             ),
             'share_stats_twitter_learning': get_tw_share_url(
-                url='http://mzl.la/1hysQTb',
+                url='http://mzl.la/1ty5Ffu',
                 text='#Firefox {}'
                      .format(_('teaches digital skills to millions of people to help them move '
                                'from using the Web to actively making it.')),
             ),
             'share_stats_twitter_control': get_tw_share_url(
-                url='http://mzl.la/PXXz0j',
+                url='http://mzl.la/Qvn21j',
                 text='#Firefox {}'
                      .format(_('leads the way in giving users greater control online. '
                                'They pioneered features like Do Not Track and Lightbeam.')),
@@ -106,12 +106,12 @@ class GlowView(TemplateView):
             'share_map_facebook': get_fb_share_url('http://mzl.la/1oKbBCb'),
             'share_video_facebook': get_fb_share_url('http://mzl.la/1hpOzMW'),
             'share_stats_facebook': get_fb_share_url('http://mzl.la/1sxET6z'),
-            'share_stats_facebook_privacy': get_fb_share_url('http://mzl.la/1n14u6H'),
-            'share_stats_facebook_opportunity': get_fb_share_url('http://mzl.la/1hGzq48'),
-            'share_stats_facebook_access': get_fb_share_url('http://mzl.la/1qo3icx'),
-            'share_stats_facebook_freedom': get_fb_share_url('http://mzl.la/OPDh7Y'),
-            'share_stats_facebook_learning': get_fb_share_url('http://mzl.la/1n14T8X'),
-            'share_stats_facebook_control': get_fb_share_url('http://mzl.la/R9IaL7'),
+            'share_stats_facebook_privacy': get_fb_share_url('http://mzl.la/1kXIpU8'),
+            'share_stats_facebook_opportunity': get_fb_share_url('http://mzl.la/1jEWeE7'),
+            'share_stats_facebook_access': get_fb_share_url('http://mzl.la/1ty5r7W'),
+            'share_stats_facebook_freedom': get_fb_share_url('http://mzl.la/1icscuJ'),
+            'share_stats_facebook_learning': get_fb_share_url('http://mzl.la/1rkanJU'),
+            'share_stats_facebook_control': get_fb_share_url('http://mzl.la/1k45KCq'),
             'count_footnote': COUNT_FOOTNOTE.format(_('What does this number mean?')),
         })
         return context
