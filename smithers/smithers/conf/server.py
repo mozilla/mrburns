affca524ec3163716a78a365ac5781ec3130ad50
Paul McLanahan <pmac@mozilla.com>
2014-04-22 12:22:00 -0400

    Disable country minimum share limit.

--- smithers/smithers/conf/server.py
+++ smithers/smithers/conf/server.py
@@ -5,3 +5,5 @@ GEOIP_DB_FILE = '/usr/local/share/GeoIP/GeoIP2-City.mmdb'
 STATSD_HOST = 'graphite1.private.phx1.mozilla.com'
 STATSD_PORT = 8125
 STATSD_PREFIX = 'glow-workers-{}'.format(getenv('DJANGO_SERVER_ENV'))
+
+COUNTRY_MIN_SHARE = 1  # basically off
