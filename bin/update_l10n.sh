53f7a0e02ecbbd52a71c07ef8b01e847a2c82f6f
Paul McLanahan <pmac@mozilla.com>
2014-04-23 11:42:20 -0400

    Further improve l10n error reporting.
    Also improve output of deploy.

--- bin/update_l10n.sh
+++ bin/update_l10n.sh
@@ -3,13 +3,22 @@
 source /data/venvs/mrburns/bin/activate
 source /home/mrburns/mrburns_env
 
-svn up locale
+#svn up locale
 
+SUBJECT="[Glow2014-${DJANGO_SERVER_ENV}] l10n update error"
 REV_FILE=".locale_revision"
 test ! -e $REV_FILE && touch $REV_FILE
 
 locale_revision=$(cat $REV_FILE)
 new_revision=$(svnversion -cn locale | cut -d ':' -f 2)
+
+function report_error() {
+    if [ -n "$L10N_ERROR_EMAILS" ]; then
+        printf "$1" | mail -s "$SUBJECT" "$L10N_ERROR_EMAILS"
+    else
+        printf "$1"
+    fi
+}
 
 if [ "$locale_revision" != "$new_revision" ]; then
     echo $new_revision > $REV_FILE
@@ -19,10 +28,10 @@ if [ "$locale_revision" != "$new_revision" ]; then
         if [ $? -eq 0 ]; then
             sudo supervisorctl restart mrburns
         else
-            echo "Some .po files failed to compile in r${new_revision}.\n\n$errors" | \
-                mail -s "Glow l10n error" pmac@mozilla.com
+            report_error "Some .po files failed to compile in r${new_revision}.\n\n$errors"
+
         fi
     else
-        echo "Dennis found a problem with the .po files in r${new_revision}.\n\n$errors" | mail -s "Glow l10n error" pmac@mozilla.com
+        report_error "Dennis found a problem with the .po files in r${new_revision}.\n\n$errors"
     fi
 fi
