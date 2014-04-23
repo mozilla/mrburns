dbe3f9e2093e4d54c5411555f256277628defee0
Paul McLanahan <pmac@mozilla.com>
2014-04-23 12:38:31 -0400

    Improve og:url value generation.

--- mrburns/main/context_processors.py
+++ mrburns/main/context_processors.py
@@ -4,5 +4,6 @@ from django.conf import settings
 def glow_variables(request):
     return {
         'MAP_DATA_URL': settings.MAP_DATA_URL,
-        'OG_ABS_URL': request.build_absolute_uri().split('?')[0],
+        'OG_ABS_URL': 'https://{}{}'.format(request.get_host(),
+                                            request.path)
     }
