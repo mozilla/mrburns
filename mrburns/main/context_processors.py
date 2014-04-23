dc6322b8117761a44ecb44db18d335b661373eb5
Paul McLanahan <pmac@mozilla.com>
2014-04-23 12:10:44 -0400

    Fix og:url value to be the real full URI for the page.

--- mrburns/main/context_processors.py
+++ mrburns/main/context_processors.py
@@ -4,4 +4,5 @@ from django.conf import settings
 def glow_variables(request):
     return {
         'MAP_DATA_URL': settings.MAP_DATA_URL,
+        'OG_ABS_URL': request.build_absolute_uri().split('?')[0],
     }
