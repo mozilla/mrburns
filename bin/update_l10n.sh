4018668703bb00929172a1345027568516c00d1e
Paul McLanahan <pmac@mozilla.com>
2014-04-23 10:41:12 -0400

    Improve l10n error reporting.

--- bin/update_l10n.sh
+++ bin/update_l10n.sh
@@ -13,13 +13,16 @@ new_revision=$(svnversion -cn locale | cut -d ':' -f 2)
 
 if [ "$locale_revision" != "$new_revision" ]; then
     echo $new_revision > $REV_FILE
-    if dennis-cmd lint locale; then
-        if python manage.py compilemessages; then
+    errors=$(dennis-cmd lint locale)
+    if [ $? -eq 0 ]; then
+        errors=$(python manage.py compilemessages 2>&1 > /dev/null)
+        if [ $? -eq 0 ]; then
             sudo supervisorctl restart mrburns
         else
-            echo "The .po files failed to compile in r${new_revision}." | mail -s "Glow l10n error" pmac@mozilla.com
+            echo "Some .po files failed to compile in r${new_revision}.\n\n$errors" | \
+                mail -s "Glow l10n error" pmac@mozilla.com
         fi
     else
-        echo "Dennis found a problem with the .po files in r${new_revision}." | mail -s "Glow l10n error" pmac@mozilla.com
+        echo "Dennis found a problem with the .po files in r${new_revision}.\n\n$errors" | mail -s "Glow l10n error" pmac@mozilla.com
     fi
 fi
