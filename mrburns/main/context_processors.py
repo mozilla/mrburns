1a5e55e1a0354182a2b23cd51292cb1cd3c3a88d
Josh Mize <jgmize@gmail.com>
2014-05-01 17:38:08 +0000

    Handle unicode paths in glow_variables

--- mrburns/main/context_processors.py
+++ mrburns/main/context_processors.py
@@ -4,6 +4,6 @@ from django.conf import settings
 def glow_variables(request):
     return {
         'MAP_DATA_URL': settings.MAP_DATA_URL,
-        'OG_ABS_URL': 'https://{}{}'.format(request.get_host(),
-                                            request.path)
+        'OG_ABS_URL': u'https://{}{}'.format(request.get_host(),
+                                             request.path)
     }
