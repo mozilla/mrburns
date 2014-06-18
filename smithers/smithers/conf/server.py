50f8efd7bcbf032fd0295c460b98640d0bf6c1ed
Paul McLanahan <pmac@mozilla.com>
2014-06-18 15:36:11 -0400

    Make statsd host configurable via env.

--- smithers/smithers/conf/server.py
+++ smithers/smithers/conf/server.py
@@ -2,7 +2,7 @@ from os import getenv
 
 GEOIP_DB_FILE = '/usr/local/share/GeoIP/GeoIP2-City.mmdb'
 
-STATSD_HOST = 'graphite1.private.phx1.mozilla.com'
+STATSD_HOST = getenv('STATSD_HOST', 'graphite1.private.phx1.mozilla.com')
 STATSD_PORT = 8125
 STATSD_PREFIX = 'glow-workers-{}'.format(getenv('DJANGO_SERVER_ENV'))
 
